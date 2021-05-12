ac723acef64b5d2a397e22ab78548032a5ca75d0
Kriti <60443387+kriti-ixix@users.noreply.github.com>
2021-05-12 16:03:12 +0530

    Add files via upload

---
+++ MyStrings.java
@@ -0,0 +1,70 @@
+class MyStrings
+{
+	public static void main(String[] args)
+	{
+		/*
+		String firstName = "Kriti";
+		String lastName = "Bhatia";
+		String nickName = "kriti";
+
+		Getting a single character from string:
+		char myChar = firstName.charAt(1);
+
+		String concatenation:
+		String fullName = firstName + " " + lastName;
+		String fullName = firstName.concat(lastName);
+		String formattedName = firstName.concat(" ").concat(lastName);
+
+		Comparing two strings:
+		if (firstName.equals(nickName))
+		{
+			System.out.println("They are the same");
+		}
+		else
+		{
+			System.out.println("They are not the same");
+		}
+
+		Comparing while ignoring case
+		if (firstName.equalsIgnoreCase(nickName))
+		{
+			System.out.println("They are the same");
+		}
+		else
+		{
+			System.out.println("They are not the same");
+		}
+
+		Converting upper and lower case
+		String userEmail = "KrItI@GMaIL.COM";
+		String actualEmail = userEmail.toLowerCase();
+		String upperEmail = userEmail.toUpperCase();
+
+		System.out.println(actualEmail);
+		System.out.println(upperEmail);
+
+		System.out.println(firstName.length());
+		firstName = firstName + " " + lastName;
+		System.out.println(firstName.length());
+
+		Splitting a string
+		String cities = "London Paris NewYork Delhi LosAngeles";
+		String[] citiesArray = cities.split(" ");
+
+		System.out.println(java.util.Arrays.toString(citiesArray));
+
+		String fullName = "Kriti Bhatia";
+		String mySubstring = fullName.substring(4);
+
+		System.out.println(mySubstring);
+
+		String myName = "Kriti";
+
+		for (int i=0; i<myName.length(); i++)
+		{
+			char myChar = myName.charAt(i);
+			System.out.println(myChar);
+		}
+		*/
+	}
+}
